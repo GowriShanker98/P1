aff5eebd81205cb99eda1249f0435d5f66bb0232
Star Hero <gowrishankarkoothappan@gmail.com>
2020-07-17 19:01:36 +0530

    comments

--- P1/views.py
+++ P1/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 #create your views here.
 def index(request):
     return HttpResponse('Hello World')
 
-
 def home(request):
     return HttpResponse('<h1>Welcome to Home Page</h1>')
+
+def html_demo1(request):
+    return render(request,"sample.html")
+
+def html_demo2(request):
+    return render(request,"sample1.html")
