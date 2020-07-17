4becd9178285e8689a4f888eca1987e6cd0b1cfc
K.Gowri Shanker <65294968+GowriShanker98@users.noreply.github.com>
2020-07-17 20:11:59 +0530

    Update settings.py

--- P1/settings.py
+++ P1/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = "C:\\Users\\Star Hero\\Desktop\\New\\New Django\\P1\\templates"
+TEMPLATE_DIR = "H:\\P1\\templates"
 
 
 # Quick-start development settings - unsuitable for production
